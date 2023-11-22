1835ff35301d0d18a3612b2f2132ff09d2008bc9
zevi bem <zevibem@gmail.com>
2023-11-22 07:05:15 +0200

    chack if i did this good

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,19 +4,14 @@ import IconButton from '@mui/material/IconButton';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import TwitterIcon from '@mui/icons-material/Twitter';
-import { Link } from 'react-router-dom';
      export default function Footer() {
        return (
-        //  <footer className={styles.footer}>
-        //     <h1>Footer</h1>
-        //  </footer>
       <AppBar position="static" className={styles.footer}>
         <Container sx={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
           <Toolbar>
             <Typography variant="body1" color="inherit">
               WWW.@banner.com
             </Typography>
-            <br />
             <IconButton>
               <InstagramIcon />
             </IconButton>
