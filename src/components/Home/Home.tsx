0527bb525b4af90dc5b5b521e202d1ce0852beff
Ariel Zamir <arielzamir100@gmail.com>
2023-11-23 14:16:35 +0200

    Merged the conflicts

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,38 +1,28 @@
 import styles from "./Home.module.css";
-<<<<<<< HEAD
-import SignIn from "../Login/SignIn";
+import SignIn from "../SignIn/SignIn";
 import Popup from "../Popup/Popup";
-import GetAllBanners from "../Banner/GetAllBanners/GetAllBanners";
-=======
 import { RootState } from "../../redux/store";
 import { useSelector } from "react-redux";
 import HelloWorld from "../TestHelloWorld/TestHelloWorld";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
-import GetAllBanners from "../Banner/GetAllBanners";
->>>>>>> 487169212043091b329f1578ab5d905d3f4b2e36
 
 export default function Home() {
-  
   const navigate = useNavigate();
   const user = useSelector((state: RootState) => state.user.user);
 
   useEffect(() => {
     console.log(user?.isConnected);
-    !(user?.isConnected) && navigate('/login')
-  }, [navigate, user?.isConnected])
-  
+    !user?.isConnected && navigate("/login");
+  }, [navigate, user?.isConnected]);
+
   return (
     <div className={styles.context}>
       <h1>Home</h1>
-<<<<<<< HEAD
       <Popup>
         <SignIn />
       </Popup>
-=======
-      <HelloWorld/>
->>>>>>> 487169212043091b329f1578ab5d905d3f4b2e36
-      <GetAllBanners />
+      <HelloWorld />
     </div>
   );
 }
