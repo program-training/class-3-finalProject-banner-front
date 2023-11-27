38aadbb795033f6cb7d5f15cf3e6ff65ebe0a031
Ariel Zamir <arielzamir100@gmail.com>
2023-11-27 12:08:26 +0200

    Changed the routing to latest versioon of react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,7 @@
 import "./App.css";
-import HelloWorld from "./components/TestHelloWorld/TestHelloWorld";
-import CssBaseline from "@mui/material/CssBaseline";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import BaseLayout from "./components/BaseLayout/BaseLayout";
-import Home from "./components/Home/Home";
-import SignIn from "./components/SignIn/SignIn";
-import SignUp from "./components/SignUp/SignUp";
-import AddNewRecommendedBanner from "./components/AddNewRecommendedBanner/AddNewRecommendedBanner";
 
 function App() {
-  return (
-    <>
-      <CssBaseline />
-      <BrowserRouter>
-        <Routes>
-          <Route element={<BaseLayout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/addNewRec" element={<AddNewRecommendedBanner />} />
-            <Route path="/login" element={<SignIn />} />
-            <Route path="/register" element={<SignUp />} />
-            <Route path="/HelloWorld" element={<HelloWorld />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
