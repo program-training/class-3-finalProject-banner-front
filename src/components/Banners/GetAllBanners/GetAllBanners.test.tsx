bbf6de1e1ff0096f0249f6bd400a76b06b654721
Ariel Zamir <arielzamir100@gmail.com>
2023-11-29 21:12:51 +0200

    Deleted the test component

--- src/components/Banners/GetAllBanners/GetAllBanners.test.tsx
+++
@@ -1,8 +0,0 @@
-import { describe, expect, test } from "vitest";
-import GetAllBanners from "./GetAllBanners";
-
-describe("GetAllBanners", () => {
-  test("Renders the GetAllBanners comp", () => {
-    //Arrange
-  });
-});
