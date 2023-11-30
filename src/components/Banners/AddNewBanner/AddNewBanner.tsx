571f5b9e880f0168a7698aeaa70235e8099fd440
ariHlfgut <141528257+ariHlfgut@users.noreply.github.com>
2023-11-30 14:25:33 +0200

    Update AddNewBanner.tsx
    delete //

--- src/components/Banners/AddNewBanner/AddNewBanner.tsx
+++ src/components/Banners/AddNewBanner/AddNewBanner.tsx
@@ -89,7 +89,7 @@ const AddNewBanner = () => {
             {...register("url", { required: true })}
             error={!!errors.url}
             helperText={errors.url?.message}
-            // value={formData.url}
+             value={formData.url}
             onChange={(event) => handleInputChange("url", event.target.value)}
             label="URL"
           />
@@ -100,7 +100,7 @@ const AddNewBanner = () => {
           <TextField
             {...register("title")}
             label="Title"
-            // value={formData.title}
+             value={formData.title}
             onChange={(event) => handleInputChange("title", event.target.value)}
           />
         </FormControl>
@@ -110,7 +110,7 @@ const AddNewBanner = () => {
           <TextField
             {...register("text")}
             label="Text"
-            // value={formData.text}
+            value={formData.text}
             onChange={(event) => handleInputChange("text", event.target.value)}
           />
         </FormControl>
