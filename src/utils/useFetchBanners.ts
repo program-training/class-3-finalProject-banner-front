f3df4f15560eb67aac8ee32a96883fe2456e97ef
Ariel Zamir <arielzamir100@gmail.com>
2023-12-11 13:38:54 +0200

    Adjusted the useFetchBanners.ts to handle graphql queries

--- src/utils/useFetchBanners.ts
+++ src/utils/useFetchBanners.ts
@@ -1,31 +1,49 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
+import { useQuery } from "@apollo/client";
+import {
+  GET_BANNERS,
+  GET_BANNER_BY_ID,
+  GET_ALL_CATEGORIES,
+} from "../graphql/bannerQueries";
 import { BannersInterFace, CategoryInterface } from "./interfaces";
 
 export const useFetchBanners = (url: string) => {
+  const {
+    loading: bannersLoading,
+    error: bannersError,
+    data: bannersData,
+  } = useQuery(GET_BANNERS, {
+    variables: { url },
+  });
+
+  const {
+    loading: bannerByIdLoading,
+    error: bannerByIdError,
+    data: bannerByIdData,
+  } = useQuery(GET_BANNER_BY_ID, {
+    variables: { url },
+  });
+
+  const {
+    loading: allCategoriesLoading,
+    error: allCategoriesError,
+    data: allCategoriesData,
+  } = useQuery(GET_ALL_CATEGORIES, {
+    variables: { url },
+  });
+
   const [allBanners, setAllBanners] = useState<BannersInterFace[]>([]);
   const [bannerById, setBannerById] = useState<BannersInterFace>();
   const [allCategories, setAllCategories] = useState<CategoryInterface[]>([]);
 
   useEffect(() => {
-    const fetchBanners = async () => {
-      try {
-        const res = await axios.get(`${import.meta.env.VITE_BASE_URL}${url}`);
-        if (res.status < 300 && res.status >= 200) {
-          const data = res.data;
-          setAllBanners(data);
-          setAllCategories(data);
-          setBannerById(data);
-        } else {
-          console.log("error fetching banners", res.status);
-        }
-      } catch (error) {
-        console.error(error);
-        throw error;
-      }
-    };
-    fetchBanners();
-  }, [url]);
+    if (!bannersLoading && !bannersError && bannersData) {
+      const { banners } = bannersData;
+      setAllBanners(banners);
+      setAllCategories(banners);
+      setBannerById(banners);
+    }
+  }, [bannersLoading, bannersError, bannersData]);
 
   return {
     allBanners,
@@ -34,5 +52,11 @@ export const useFetchBanners = (url: string) => {
     setAllCategories,
     bannerById,
     setBannerById,
+    bannersLoading,
+    bannersError,
+    bannerByIdLoading,
+    bannerByIdError,
+    allCategoriesLoading,
+    allCategoriesError,
   };
 };
